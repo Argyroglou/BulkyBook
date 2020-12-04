2654e4a17b90ec5f1515c68d2108901d62eda443
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2020-12-04 11:37:29 +0200

    implementation of repository and repository interface

--- BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -9,18 +9,23 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         T Get(int id);
+
         IEnumerable<T> GetAll(
             Expression<Func<T, bool>> filter = null,
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
             string includeProperties = null
             );
-        IEnumerable<T> GetFirstOrDefault(
+
+        T GetFirstOrDefault(
             Expression<Func<T, bool>> filter = null,
             string includeProperties = null
             );
+
         void Add(T entity);
         void Remove(int id);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
+
+
     }
 }
