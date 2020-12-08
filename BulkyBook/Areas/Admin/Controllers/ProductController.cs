c682220f4deb3d550853dad8175662aa008da7ad
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2020-12-08 09:57:03 +0200

    Implementation of Product

--- BulkyBook/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -1,31 +1,33 @@
-﻿using BulkyBook.DataAccess.Repository.IRepository;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace BulkyBook.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _hostEnviroment;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
         public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
-            _hostEnviroment = hostEnvironment;
+            _hostEnvironment = hostEnvironment;
         }
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new ProductVM()
@@ -54,6 +56,7 @@ namespace BulkyBook.Areas.Admin.Controllers
                 return NotFound();
             }
             return View(productVM);
+
         }
 
         [HttpPost]
@@ -62,29 +65,28 @@ namespace BulkyBook.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                string webRootPath = _hostEnviroment.WebRootPath;
+                string webRootPath = _hostEnvironment.WebRootPath;
                 var files = HttpContext.Request.Form.Files;
-                if (files.Count() > 0)
+                if (files.Count > 0)
                 {
                     string fileName = Guid.NewGuid().ToString();
                     var uploads = Path.Combine(webRootPath, @"images\products");
-                    var extension = Path.GetExtension(files[0].Name);
+                    var extenstion = Path.GetExtension(files[0].FileName);
 
                     if (productVM.Product.ImageUrl != null)
                     {
-                        //this is edit and we need to remove image
-
+                        //this is an edit and we need to remove old image
                         var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
                         if (System.IO.File.Exists(imagePath))
                         {
                             System.IO.File.Delete(imagePath);
                         }
                     }
-                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extenstion), FileMode.Create))
                     {
-                        files[0].CopyTo(fileStreams);
+                        files[0].CopyTo(filesStreams);
                     }
-                    productVM.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                    productVM.Product.ImageUrl = @"\images\products\" + fileName + extenstion;
                 }
                 else
                 {
@@ -95,9 +97,12 @@ namespace BulkyBook.Areas.Admin.Controllers
                         productVM.Product.ImageUrl = objFromDb.ImageUrl;
                     }
                 }
+
+
                 if (productVM.Product.Id == 0)
                 {
                     _unitOfWork.Product.Add(productVM.Product);
+
                 }
                 else
                 {
@@ -126,13 +131,13 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View(productVM);
         }
 
+
         #region API CALLS
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
-
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = allObj });
         }
 
@@ -144,7 +149,7 @@ namespace BulkyBook.Areas.Admin.Controllers
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
-            string webRootPath = _hostEnviroment.WebRootPath;
+            string webRootPath = _hostEnvironment.WebRootPath;
             var imagePath = Path.Combine(webRootPath, objFromDb.ImageUrl.TrimStart('\\'));
             if (System.IO.File.Exists(imagePath))
             {
@@ -153,7 +158,9 @@ namespace BulkyBook.Areas.Admin.Controllers
             _unitOfWork.Product.Remove(objFromDb);
             _unitOfWork.Save();
             return Json(new { success = true, message = "Delete Successful" });
+
         }
+
         #endregion
     }
 }
