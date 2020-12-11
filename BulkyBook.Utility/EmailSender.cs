1b95ed01820a9771a7484cfe263a0b35421a1891
Basilis Argyroglou <b.argyroglou@metronsa.eu>
2020-12-11 18:10:38 +0200

    Send Grid with bugs

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.Utility
     public class EmailSender : IEmailSender
     {
         private readonly EmailOptions emailOptions;
+
         public EmailSender(IOptions<EmailOptions> options)
         {
             emailOptions = options.Value;
@@ -20,13 +21,12 @@ namespace BulkyBook.Utility
         {
             return Execute(emailOptions.SendGridKey, subject, htmlMessage, email);
         }
-        private Task Execute(string sendGridKey, string subject, string message, string email)
+        private Task Execute(string sendGridKEy, string subject, string message, string email)
         {
-            var client = new SendGridClient(sendGridKey);
-            var from = new EmailAddress("admin@bulky.com", "BulkyBooks");
+            var client = new SendGridClient(sendGridKEy);
+            var from = new EmailAddress("admin@bulky.com", "Bulky Books");
             var to = new EmailAddress(email, "End User");
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, message,"");
-            
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
             return client.SendEmailAsync(msg);
         }
     }
